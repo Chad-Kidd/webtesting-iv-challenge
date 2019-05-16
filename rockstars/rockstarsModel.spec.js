3f9d37ef7756709bd4c9626e88b205a1c1d67c75
chad kidd <ckiddwebdev@gmail.com>
2019-05-16 15:31:45 -0700

    Second test for rockstarsModel PASSED

--- rockstars/rockstarsModel.spec.js
+++ rockstars/rockstarsModel.spec.js
@@ -2,6 +2,10 @@ const db = require('../data/dbConfig');
 const Rockstars = require('../rockstars/rockstarsModel');
 
 describe('rockstars model', () => {
+  afterEach(async () => {
+    await db('rockstars').truncate();
+  });
+
   describe('insert()', () => {
     it('should insert provided rockstar',  async () => {
      await Rockstars.insert({name: 'Bowie'});
@@ -10,6 +14,16 @@ describe('rockstars model', () => {
 
      expect(rockstars).toHaveLength(1);
     })
+    it('should insert provided rockstar', async () => {
+      let rockstar = await Rockstars.insert({ name: 'Bowie' });
+      expect(rockstar.name).toBe('Bowie');
+
+      rockstar = await Rockstars.insert({ name: 'Slash' });
+      expect(rockstar.name).toBe('Slash');
+
+      const rockstars = await db('rockstars');
+
+      expect(rockstars).toHaveLength(2);
+    });
   })
-  
 })
